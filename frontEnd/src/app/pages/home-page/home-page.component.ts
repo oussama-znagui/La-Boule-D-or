c364291abd553902172b49baf4fef2ccc511f5f9
znagui <znagui.info@gmail.com>
2025-03-01 20:37:58 +0000

    creation de nav-bar et hero section

---
+++ frontEnd/src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from "../../shared/components/nav-bar/nav-bar.component";
+import { HeroSectionComponent } from "../../features/home/components/hero-section/hero-section.component";
+
+@Component({
+  selector: 'app-home-page',
+  imports: [NavBarComponent, HeroSectionComponent],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.css'
+})
+export class HomePageComponent {
+
+}
