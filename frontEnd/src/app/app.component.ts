0fc856e9ffdd91f25793fd5da38efc64de9e8bdc
znagui <znagui.info@gmail.com>
2025-03-02 23:13:27 +0000

    pages des tournoi avec la barre de recherche

--- frontEnd/src/app/app.component.ts
+++ frontEnd/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { TournamentsPageComponent } from "./pages/tournaments-page/tournaments-page.component";
+import { NavBarComponent } from "./shared/components/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomePageComponent, TournamentsPageComponent],
+  imports: [RouterOutlet, HomePageComponent, TournamentsPageComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontEnd/src/app/pages/tournaments-page/tournaments-page.component.ts
+++ frontEnd/src/app/pages/tournaments-page/tournaments-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TournamentsListComponent } from "../../features/tournaments-page/components/tournaments-list/tournaments-list.component";
 
 @Component({
   selector: 'app-tournaments-page',
-  imports: [],
+  imports: [TournamentsListComponent],
   templateUrl: './tournaments-page.component.html',
   styleUrl: './tournaments-page.component.css'
 })
