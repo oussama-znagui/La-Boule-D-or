d61402a842aa298352c0cd1538df91fb41b7c9d5
znagui <znagui.info@gmail.com>
2025-03-04 11:01:23 +0000

    Affichage des details du tournoi (joueurs, arbittre, reglements ..)

--- frontEnd/src/app/pages/tournament-page/tournament-page.component.ts
+++ frontEnd/src/app/pages/tournament-page/tournament-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TournamentDetailsComponent } from '../../features/tournament-page/components/tournament-details/tournament-details.component';
+import { PlayersListComponent } from "../../features/tournament-page/components/players-list/players-list.component";
 
 @Component({
   selector: 'app-tournament-page',
-  imports: [TournamentDetailsComponent],
+  imports: [TournamentDetailsComponent, PlayersListComponent],
   templateUrl: './tournament-page.component.html',
   styleUrl: './tournament-page.component.css'
 })
