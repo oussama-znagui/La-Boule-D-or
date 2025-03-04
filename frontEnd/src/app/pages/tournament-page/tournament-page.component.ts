32ab48ed4640525595731807adca9ce24fd01db0
znagui <znagui.info@gmail.com>
2025-03-04 10:29:00 +0000

    Affichage des details du tournoi

--- frontEnd/src/app/pages/tournament-page/tournament-page.component.ts
+++ frontEnd/src/app/pages/tournament-page/tournament-page.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { TournamentDetailsComponent } from '../../features/tournament-page/components/tournament-details/tournament-details.component';
 
 @Component({
   selector: 'app-tournament-page',
-  imports: [],
+  imports: [TournamentDetailsComponent],
   templateUrl: './tournament-page.component.html',
   styleUrl: './tournament-page.component.css'
 })
 export class TournamentPageComponent {
 
+   activeTab = "details";
+
+  setActiveTab(tab: string): void {
+    this.activeTab = tab
+  }
 }
