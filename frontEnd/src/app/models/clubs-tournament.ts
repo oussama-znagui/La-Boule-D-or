5d99c6bd7c6530f876b4fba032f699ba0fcffd5f
znagui <znagui.info@gmail.com>
2025-03-04 12:11:01 +0000

    creation des model de projet

---
+++ frontEnd/src/app/models/clubs-tournament.ts
@@ -0,0 +1,7 @@
+import { Club } from "./club";
+import { Tournament } from "./tournament";
+
+export interface ClubsTournament extends Tournament {
+    numberOfTeams: number;
+    clubs: Club[];
+}
